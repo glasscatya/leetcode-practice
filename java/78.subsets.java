4457a6a88838e6195b10a9ab4d0e5d4fcdd9136a
yufang <2721381743@qq.com>
2024-09-05 23:01:19 +0800

    78

--- java/78.subsets.java
+++ java/78.subsets.java
@@ -5,28 +5,48 @@
  * [78] 子集
  */
 
-
 // @lcpr-template-start
 
 // @lcpr-template-end
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
+    private List<List<Integer>> result = new ArrayList<>();
+
+    private List<Integer> path = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        
+        backtrack(0, nums);
+        return result;
+    }
+
+    public void backtrack(int startIndex, int[] nums) {
+        result.add(new ArrayList<>(path));
+        // 终止条件 startIndex == nums.len
+        // 本题不写终止条件 直接再for循环后面写return也是可以的
+        if (startIndex == nums.length) {
+            return;
+        }
+
+        for (int i = startIndex; i < nums.length; i++) {
+            path.add(nums[i]);
+            backtrack(i + 1, nums);
+            path.remove(path.size() - 1);
+        }
     }
 }
 // @lc code=end
 
-
-
 /*
-// @lcpr case=start
-// [1,2,3]\n
-// @lcpr case=end
-
-// @lcpr case=start
-// [0]\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // [1,2,3]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [0]\n
+ * // @lcpr case=end
+ * 
  */
-
