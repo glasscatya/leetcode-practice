029c302c154c455c7403b11fbf786277c90e1815
yufang <2721381743@qq.com>
2024-09-29 16:21:15 +0800

    106 first test

--- java/106.construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ java/106.construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -5,7 +5,6 @@
  * [106] 从中序与后序遍历序列构造二叉树
  */
 
-
 // @lcpr-template-start
 
 // @lcpr-template-end
@@ -26,49 +25,72 @@
  * }
  */
 
- import java.util.HashMap;
- import java.util.Map;
- 
- class Solution {
-     Map<Integer, Integer> map;  // 方便根据数值查找位置
-     public TreeNode buildTree(int[] inorder, int[] postorder) {
-         map = new HashMap<>();
-         for (int i = 0; i < inorder.length; i++) { // 用map保存中序序列的数值对应位置
-             map.put(inorder[i], i);
-         }
- 
-         return findNode(inorder,  0, inorder.length, postorder,0, postorder.length);  // 前闭后开
-     }
- 
-     public TreeNode findNode(int[] inorder, int inBegin, int inEnd, int[] postorder, int postBegin, int postEnd) {
-         // 参数里的范围都是前闭后开
-         if (inBegin >= inEnd || postBegin >= postEnd) {  // 不满足左闭右开，说明没有元素，返回空树
-             return null;
-         }
-         int rootIndex = map.get(postorder[postEnd - 1]);  // 找到后序遍历的最后一个元素在中序遍历中的位置
-         TreeNode root = new TreeNode(inorder[rootIndex]);  // 构造结点
-         int lenOfLeft = rootIndex - inBegin;  // 保存中序左子树个数，用来确定后序数列的个数
-         if(postEnd == 1) return root;
-         root.left = findNode(inorder, inBegin, rootIndex,
-                 postorder, postBegin, postBegin + lenOfLeft);
-         root.right = findNode(inorder, rootIndex + 1, inEnd,
-                 postorder, postBegin + lenOfLeft, postEnd - 1);
- 
-         return root;
-     }
- }
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    private int treeVal;
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        // 都为空说明是空
+        if (postorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
+        return ReceTree(inorder, postorder, 0, inorder.length, 0, postorder.length);
+    }
+
+    public TreeNode ReceTree(int[] inorder, int[] postorder, int inorderBegin, int inorderEnd, int postorderBegin,
+            int postorderEnd) {
+        // 终止条件
+        if (postorderBegin == postorderEnd) {
+            return null;
+        }
+        // 将postorder最后一个数放入树中
+        treeVal = postorder[postorderEnd - 1];
+        TreeNode root = new TreeNode(treeVal);
+
+        // 开始切割
+        // 先切割中序
+        int delimiterIndex;
+        for (delimiterIndex = inorderBegin; delimiterIndex < inorderEnd; delimiterIndex++) {
+            if (inorder[delimiterIndex] == treeVal) {
+                break;
+            }
+        }
+        // 中序左边
+        int inorderLeftBegin = inorderBegin;
+        int inorderLeftEnd = delimiterIndex;
+        // 中序右边
+        int inorderRightBegin = delimiterIndex + 1;
+        int inorderRightEnd = inorderEnd;
+
+        // 再切割后序
+        // 后序左边 左闭右闭 [ , )
+        int postorderLeftBegin = postorderBegin;
+        // 后序左边的终点 = 后序起点 + 中序左边的距离
+        int postorderLeftEnd = postorderBegin + (delimiterIndex - inorderBegin);
+        // 后序右边
+        int postorderRightBegin = postorderLeftEnd;
+        int postorderRightEnd = postorderEnd - 1;
+
+        // 递归 左
+        root.left = ReceTree(inorder, postorder, inorderLeftBegin, inorderLeftEnd, postorderLeftBegin,
+                postorderLeftEnd);
+        root.right = ReceTree(inorder, postorder, inorderRightBegin, inorderRightEnd, postorderRightBegin,
+                postorderRightEnd);
+
+        return root;
+    }
+}
 // @lc code=end
 
-
-
 /*
-// @lcpr case=start
-// [9,3,15,20,7]\n[9,15,7,20,3]\n
-// @lcpr case=end
-
-// @lcpr case=start
-// [-1]\n[-1]\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // [9,3,15,20,7]\n[9,15,7,20,3]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [-1]\n[-1]\n
+ * // @lcpr case=end
+ * 
  */
-
