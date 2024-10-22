70e979f42bb1e2c56270117f4c08eb575300bb59
yufang <2721381743@qq.com>
2024-10-22 10:25:45 +0800

    new Set resolve

--- java/46.permutations.java
+++ java/46.permutations.java
@@ -20,7 +20,7 @@ class Solution {
         backTrack(nums, used);
         return result;
     }
-
+    //使用数组
     public void backTrack(int[] nums, int[] used) {
         if (path.size() == nums.length) {
             result.add(new ArrayList<>(path));
@@ -35,6 +35,27 @@ class Solution {
             used[i] = 1;
             backTrack(nums, used);
             path.remove(path.size() - 1);
+        }
+    }
+
+    //使用set
+    private Set<Integer> set = new HashSet<>();
+    public void backTrack_Set(int[] nums) {
+        //收取条件
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        // 暴力 每次都循环到底 然后用set判断当前值是否存过。。。
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                continue;
+            }
+            path.add(nums[i]);
+            set.add(nums[i])
+            backTrack(nums);
+            path.remove(path.size() - 1);
+            set.remove(nums[i]);
         }
     }
 }
