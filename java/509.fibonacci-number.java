18848f6760609cc5777b81b0f9b2eb333567cba8
yufang <2721381743@qq.com>
2024-10-24 19:53:07 +0800

    509 new resolve

--- java/509.fibonacci-number.java
+++ java/509.fibonacci-number.java
@@ -18,6 +18,17 @@ class Solution {
                 .findFirst()
                 .get()[0];
     }
+
+    public int fib_dp(int n) {
+        if (n <= 1) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
 }
 // @lc code=end
 
