ccff51574bc4d16bc95706599189437601a21443
yufang <2721381743@qq.com>
2024-11-01 20:21:05 +0800

    377 java

--- java/70.climbing-stairs.java
+++ java/70.climbing-stairs.java
@@ -5,23 +5,36 @@
  * [70] 爬楼梯
  */
 
-
 // @lcpr-template-start
 
 // @lcpr-template-end
 // @lc code=start
 class Solution {
     public int climbStairs(int n) {
-        //dp[2]有意义
-        if(n <= 1) return n; 
-        //dp数组初始化
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+
+
+
+    public int climbStairs_bat(int n) {
+        // dp[2]有意义
+        if (n <= 1)
+            return n;
+        // dp数组初始化
         int[] dp = new int[n + 1];
-        //下标含义
+        // 下标含义
         dp[1] = 1;
         dp[2] = 2;
-        //递推公式
-        //遍历顺序: 从前向后
-        for(int i = 3; i <= n; i++) {
+        // 递推公式
+        // 遍历顺序: 从前向后
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
@@ -29,17 +42,13 @@ class Solution {
 }
 // @lc code=end
 
-
-
-
 /*
-// @lcpr case=start
-// 2\n
-// @lcpr case=end
-
-// @lcpr case=start
-// 3\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // 2\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // 3\n
+ * // @lcpr case=end
+ * 
  */
-
