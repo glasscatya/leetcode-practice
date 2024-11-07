933536d604522c8163d2d20d6768c06bef22435d
yufang <2721381743@qq.com>
2024-11-07 15:59:50 +0800

    Second Solution

--- java/121.best-time-to-buy-and-sell-stock.java
+++ java/121.best-time-to-buy-and-sell-stock.java
@@ -25,9 +25,20 @@ class Solution {
 
         return dp[prices.length];
     }
-
+    // 动态规划
     public int maxProfit(int[] prices) {
-        return 0;
+        int length = prices.length;
+        int[][] dp = new int[length][2];
+        // 1 代表持有某一支股票
+        dp[0][1] = -prices[0];
+        // 0 代表不持有股票
+        dp[0][0] = 0;
+        for (int i = 1; i < length; i++) {
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+        }
+
+        return dp[length - 1][0];
     }
 }
 // @lc code=end
