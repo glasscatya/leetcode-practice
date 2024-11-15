fb47022db742b96dde5af30bca9d83bd11b36346
yufang <2721381743@qq.com>
2024-11-15 11:44:42 +0800

    leetcode.1143-java

---
+++ java/1143.longest-common-subsequence.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=java
+ * @lcpr version=30204
+ *
+ * [1143] 最长公共子序列
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "abcde"\n"ace"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abc"\n"abc"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abc"\n"def"\n
+// @lcpr case=end
+
+ */
+
