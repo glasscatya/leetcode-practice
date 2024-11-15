fe1bcab0407aac84d019a092a228b4cc803b1f49
yufang <2721381743@qq.com>
2024-11-15 11:44:46 +0800

    leetcode.53-java

---
+++ java/53.maximum-subarray.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ * @lcpr version=30204
+ *
+ * [53] 最大子数组和
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public int maxSubArray0(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            if (dp[i] > result) result = dp[i];
+        }
+
+        return result;
+    }
+
+    //压缩空间
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [-2,1,-3,4,-1,2,1,-5,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5,4,-1,7,8]\n
+// @lcpr case=end
+
+ */
+
