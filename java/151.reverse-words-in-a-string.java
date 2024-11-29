e2c211e4ff98c9583e3a77826b1cf34b27cbdaa9
yufang <2721381743@qq.com>
2024-11-29 14:20:48 +0800

    new regulations

--- java/151.reverse-words-in-a-string.java
+++ java/151.reverse-words-in-a-string.java
@@ -19,18 +19,21 @@ class Solution {
         return new String(chars);
     }
 
+    //移除所有*多余*的空格
+    //使用快慢指针 ： 慢指针放在需要被替换的位置 快指针放在第一个可以替换的位置
     public char[] removeExtraSpaces(char[] chars) {
         int slow = 0;
         for (int fast = 0; fast < chars.length; fast++) {
+            // 快指针遇到可以替换的单词
             if (chars[fast] != ' ') {
                 if (slow != 0)
                     chars[slow++] = ' ';// 除了开头的单词之外，单词之间都要有一个空格
+                // 用while保证一个单词替换完成
                 while (fast < chars.length && chars[fast] != ' ') {
                     chars[slow++] = chars[fast++];
                 }
             }
         }
-       //相当于 c++ 里的 resize()
        char[] newChars = new char[slow];
        System.arraycopy(chars, 0, newChars, 0, slow); 
        return newChars;
@@ -50,6 +53,60 @@ class Solution {
 
     public void reverseEachWords(char[] chars) {
         int start = 0;
+        //交换一个单词的顺序 单词以空格为分界
+        for (int end = 0; end <= chars.length; end++) {
+            //遇到空格 就说明走到一个单词的尽头了 或者 end走到了最后一个单词的尽头
+            if (end == chars.length || chars[end] == ' ') {
+                //我实现的反转字符串是 左闭右闭的[]   一般官方库是左闭右开[)
+                reverseString(chars, start, end - 1);
+                // end刚好在空格 新单词的起始位置在空格后一格
+                start = end + 1;
+            }
+        }
+    }
+}
+
+
+/**
+class Solution {
+    public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        //移出多余的空格
+        chars = removeExtraSpaces(chars);
+        //反转字符串
+        reverseString(chars, 0, chars.length - 1);
+        //二次反转字符串中的每个单词
+        reverseEachWords(chars);
+        return new String(chars);
+    }
+
+    private char[] removeExtraSpaces(char[] chars) {
+        int slow = 0;
+        for (int fast = 0; fast < chars.length; fast++) {
+            if (chars[fast] != ' ') {
+                if (slow > 0) chars[slow++] = ' ';
+
+                while (fast < chars.length && chars[fast] != ' ') 
+                    chars[slow++] = chars[fast++];
+            }
+        }
+        char[] newChars = new char[slow];
+        System.arraycopy(chars, 0, newChars, 0, slow); 
+        return newChars;
+    }
+
+    private void reverseString(char[] chars, int s, int e) {
+        int start = s;
+        int end = e;
+        while (end > start) {
+            char tmp = chars[start];
+            chars[start++] = chars[end];
+            chars[end--] = tmp;
+        }
+    }
+
+    private void reverseEachWords(char[] chars) {
+        int start = 0;
         for (int end = 0; end <= chars.length; end++) {
             if (end == chars.length || chars[end] == ' ') {
                 reverseString(chars, start, end - 1);
@@ -58,6 +115,7 @@ class Solution {
         }
     }
 }
+ */
 // @lc code=end
 
 /*
