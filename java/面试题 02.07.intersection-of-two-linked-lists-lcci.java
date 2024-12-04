9230b1fafac13d6791fa82c66f725ab97344f79a
yufang <2721381743@qq.com>
2024-12-04 15:47:53 +0800

    leetcode.面试题 02-java

---
+++ java/面试题 02.07.intersection-of-two-linked-lists-lcci.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=面试题 02.07 lang=java
+ * @lcpr version=30204
+ *
+ * [面试题 02.07] 链表相交
+ */
+
+
+// @lcpr-template-start
+import java.util.*;
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 8\n[4,1,8,4,5]\n[5,0,1,8,4,5]\n2\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2\n[0,9,1,2,4]\n[3,2,4]\n3\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 0\n[2,6,4]\n[1,5]\n3\n2\n
+// @lcpr case=end
+
+ */
+
