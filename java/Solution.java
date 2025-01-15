43298f9366dc7116b383f0522ade27ac2e403171
yufang <2721381743@qq.com>
2025-01-15 22:55:27 +0800

    leetcode.Solution-java

---
+++ java/Solution.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ * @lcpr version=30204
+ *
+ * [141] 环形链表
+ */
+
+
+// @lcpr-template-start
+import java.util.*;
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode sent = new ListNode(0, head);
+        ListNode slow = sent;
+        ListNode fast = sent;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,0,-4]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n-1\n
+// @lcpr case=end
+
+ */
+
