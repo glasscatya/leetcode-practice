8717e6920e8d7c805aff0f75cdb2ab0f0571ce5c
yufang <2721381743@qq.com>
2025-01-20 21:46:21 +0800

    leetcode.2130-java

---
+++ java/2130.maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=2130 lang=java
+ * @lcpr version=30204
+ *
+ * [2130] 链表最大孪生和
+ */
+
+
+// @lcpr-template-start
+import java.util.*;
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [5,4,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [4,2,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,100000]\n
+// @lcpr case=end
+
+ */
+
