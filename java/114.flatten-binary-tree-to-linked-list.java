03794f7de58092260a5d9f6696449eac1be618df
yufang <2721381743@qq.com>
2025-01-25 21:12:21 +0800

    leetcode.114-java

---
+++ java/114.flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=java
+ * @lcpr version=30204
+ *
+ * [114] 二叉树展开为链表
+ */
+
+
+// @lcpr-template-start
+import java.util.*;
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode head;
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.left = null;
+        root.right = head; 
+        head = root; 
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,5,3,4,null,6]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n
+// @lcpr case=end
+
+ */
+
