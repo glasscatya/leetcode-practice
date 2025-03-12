888ba8ab3f90ed737a2110b03ddae89fcc96b03a
yufang <2721381743@qq.com>
2025-03-12 19:16:40 +0800

    leetcode.332-java

---
+++ java/332.reconstruct-itinerary.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=332 lang=java
+ * @lcpr version=30204
+ *
+ * [332] 重新安排行程
+ */
+
+
+// @lcpr-template-start
+import java.util.*;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]\n
+// @lcpr case=end
+
+ */
+
