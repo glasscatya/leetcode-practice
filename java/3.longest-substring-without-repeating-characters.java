dc5ff37c1973ef24abca90802288cdb8c24fa81b
yufang <2721381743@qq.com>
2025-03-12 19:16:39 +0800

    leetcode.3-java

---
+++ java/3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=java
+ * @lcpr version=30204
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+
+// @lcpr-template-start
+import java.util.*;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int max = 0;
+        var set = new HashSet<Character>();
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(c);
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "abcabcbb"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "bbbbb"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "pwwkew"\n
+// @lcpr case=end
+
+ */
+
